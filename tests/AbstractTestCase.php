16c9caeb8b945eeb3ca182c55a2b2f7ea59dbf68
Daniel Callaghan <dan@office>
2018-03-08 15:03:26 +0000

    Added test skeleton

---
+++ tests/AbstractTestCase.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace EdmondsCommerce\BehatHtmlContext;
+
+use PHPUnit\Framework\TestCase;
+
+abstract class AbstractTestCase extends TestCase
+{
+
+}
---
+++ tests/HtmlContextTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace EdmondsCommerce\BehatHtmlContext;
+
+class HtmlContextTest extends AbstractTestCase
+{
+    public function testClickOnTextWillFindTheTextAndClick()
+    {
+
+    }
+
+    public function testClickOnTextWillFailWhenTextIsNotPresent()
+    {
+
+    }
+
+    public function testClickOnFirstTextWillClickOnlyTheFirstText()
+    {
+
+    }
+
+    public function testClickOnFirstTextWillNotClickOnOtherTextInstances()
+    {
+
+    }
+}
