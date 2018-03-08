031ff46d8757769f3568c4fa9193be57404fbdfe
Daniel Callaghan <dan@office>
2018-03-08 15:22:02 +0000

    Added steps to set up mink and goutte

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -3,8 +3,30 @@
 namespace EdmondsCommerce\BehatHtmlContext;
 
 use PHPUnit\Framework\TestCase;
+use Behat\Mink\Driver\GoutteDriver;
+use Behat\Mink\Mink;
+use Behat\Mink\Session;
+use GuzzleHttp\Client;
 
 abstract class AbstractTestCase extends TestCase
 {
+    /**
+     * @var Session
+     */
+    protected $minkSession;
 
+    public function setUp()
+    {
+        parent::setUp();
+        $this->setUpMink();
+    }
+
+    protected function setUpMink()
+    {
+        $guzzle       = new Client();
+        $goutteClient = new \Goutte\Client();
+        $goutteClient->setClient($guzzle);
+        $goutteDriver      = new GoutteDriver($goutteClient);
+        $this->minkSession = new Session($goutteDriver);
+    }
 }
--- tests/HtmlContextTest.php
+++ tests/HtmlContextTest.php
@@ -2,11 +2,30 @@
 
 namespace EdmondsCommerce\BehatHtmlContext;
 
+
+use Behat\Mink\Mink;
+
 class HtmlContextTest extends AbstractTestCase
 {
+    /**
+     * @var HtmlContext
+     */
+    private $context;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        //Set up the mock server
+        //Set up Mink in the class
+        $context = new HTMLContext();
+        $context->setMink(new Mink([$this->minkSession]));
+    }
+
     public function testClickOnTextWillFindTheTextAndClick()
     {
-
+        $this->minkSession->visit('https://www.edmondscommerce.co.uk');
+        echo $this->minkSession->getPage()->getContent();
     }
 
     public function testClickOnTextWillFailWhenTextIsNotPresent()
