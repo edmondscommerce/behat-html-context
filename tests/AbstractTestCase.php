d3beabe35920bcfe03af94be0d58f4b7db8c9665
Tadas <tadas@edmondscommerce.co.uk>
2018-03-09 17:53:30 +0000

    Changed hostname lookup to ugly version becausse gethostbyname as sometimes returning container ip, sometimes just 0.0.0.0

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -56,6 +56,11 @@ abstract class AbstractTestCase extends TestCase
 
     protected function getContainerIp()
     {
-        return getHostByName(getHostName());
+        $commandToExecute = 'ip addr show eth0 | grep "inet\b" | awk \'{print $2}\' | cut -d/ -f1';
+
+        exec($commandToExecute, $commandOutput, $exitCode);
+
+
+        return array_pop($commandOutput);
     }
 }
